0aa14dac674bcc08d7df8d1f9e0d53dec1bb5931
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-06 10:46:00 +0100

    adds calculator tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CalculatorPage } from '../calculator/calculator'
 import { AboutPage } from '../about/about';
 
 
@@ -8,8 +8,8 @@ import { AboutPage } from '../about/about';
 })
 export class TabsPage {
 
-
-  tab2Root = AboutPage;
+  calculatorTab = CalculatorPage;
+  aboutTab = AboutPage;
  
 
   constructor() {
